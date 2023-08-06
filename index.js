58c512fd26d34a73757e6ce0b54ab3628a0fd362
Michel Escalante Álvarez <michel.escalantea@gmail.com>
2023-08-06 22:40:15 +0200

    publish app in render

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(morgan((tokens, req, res) => {
@@ -110,7 +113,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
